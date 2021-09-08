4619fd573f43cf89e592c3aa45512097a636a6c9
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-08 15:41:55 -0500

    Now calling the create fleet and herd functions in their respective classes. dino_turn and robo_turn now get user input and attack enemy based on it. Attack options are now printed by show opponent functions

--- battlefield.py
+++ battlefield.py
@@ -8,10 +8,6 @@ class Battlefield:
         self.herd = Herd()
 
     def run_game(self):
-        herd = Herd()
-        herd.create_herd()
-        fleet = Fleet()
-        fleet.create_fleet()
         self.display_welcome()
         self.battle()
 
@@ -19,24 +15,31 @@ class Battlefield:
         print('Welcome to Robots vs Dinosaurs!')
 
     def battle(self):
-        user_dino = input(
-            'Which dinosaur do you want to attack with? Type 1 for tyrannosaurus rex, 2 for triceratops, and 3 for velociraptor.')
+        user_dino_index = input(
+            'Which dinosaur do you want to attack with? Type 1 for tyrannosaurus rex, 2 for triceratops, and 3 for velociraptor: ')
+        user_dino = self.herd.dinosaurs[int(user_dino_index) - 1]
         self.dino_turn(user_dino)
-        user_robo = input(
-            'Which robot do you want to attack with? Type 1 for R2-D2, 2 for WALL-E, and 3 for Rosie.')
+
+        user_robo_index = input(
+            'Which robot do you want to attack with? Type 1 for R2-D2, 2 for WALL-E, and 3 for Rosie: ')
+        user_robo = self.fleet.robots[int(user_robo_index) - 1]
         self.robo_turn(user_robo)
 
     def dino_turn(self, dinosaur):
-        pass
+        self.show_dino_opponent_options()
+        user_attack = input(': ')
+        dinosaur.attack(self.fleet.robots[int(user_attack) - 1])
 
     def robo_turn(self, robot):
-        pass
+        self.show_robo_opponent_options()
+        user_attack = input(': ')
+        robot.attack(self.herd.dinosaurs[int(user_attack) - 1])
 
     def show_dino_opponent_options(self):
-        pass
+        print('Attack! 1 for R2-D2, 2 for WALL-E, and 3 for Rosie.')
 
     def show_robo_opponent_options(self):
-        pass
+        print('Attack! 1 for tyrannosaurus rex, 2 for triceratops, and 3 for velociraptor.')
 
     def display_winners(self):
         pass
