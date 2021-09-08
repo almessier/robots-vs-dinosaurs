5ab455b30b6dc872b48bc2a139c8cb2584560b37
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-08 17:50:34 -0500

    Added for loops to check if all the robots or dinosaurs reached 0 health. Added a for loop to check for the victor and printed the results.

--- battlefield.py
+++ battlefield.py
@@ -10,22 +10,37 @@ class Battlefield:
     def run_game(self):
         self.display_welcome()
         self.battle()
+        self.display_winners()
 
     def display_welcome(self):
         print('Welcome to Robots vs Dinosaurs!')
 
     def battle(self):
-        while():
+        while(True):
+
             user_dino_index = input(
                 'Which dinosaur do you want to attack with? Type 1 for tyrannosaurus rex, 2 for triceratops, and 3 for velociraptor: ')
             user_dino = self.herd.dinosaurs[int(user_dino_index) - 1]
             self.dino_turn(user_dino)
 
+            count = 0
+            for obj in self.fleet.robots:
+                if obj.health <= 0:
+                    count += 1
+            if count == 3:
+                break
+
             user_robo_index = input(
                 'Which robot do you want to attack with? Type 1 for R2-D2, 2 for WALL-E, and 3 for Rosie: ')
             user_robo = self.fleet.robots[int(user_robo_index) - 1]
             self.robo_turn(user_robo)
-        self.display_winners()
+
+            count = 0
+            for obj in self.herd.dinosaurs:
+                if obj.health <= 0:
+                    count += 1
+            if count == 3:
+                break
 
     def dino_turn(self, dinosaur):
         self.show_dino_opponent_options()
@@ -44,4 +59,11 @@ class Battlefield:
         print('Attack! 1 for tyrannosaurus rex, 2 for triceratops, and 3 for velociraptor.')
 
     def display_winners(self):
-        pass
+        count = 0
+        for obj in self.fleet.robots:
+            if obj.health <= 0:
+                count += 1
+        if count == 3:
+            print('The dinosaurs have defeated the robots!')
+        else:
+            print('The robots have defeated the dinosaurs!')
