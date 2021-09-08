660e9746821ddc74f0c1c991e5e7d8a99144d0b7
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-08 12:17:35 -0500

    Laid out all the properties and methods for each class from the project uml. Instantiated an object to call the run_game function in main.py.

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = 20
+
+    def attack(self, robot):
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from battlefield import Battlefield
+
+robots_vs_dinosaurs = Battlefield()
+robots_vs_dinosaurs.run_game()
