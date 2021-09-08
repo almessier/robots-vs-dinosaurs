997e12ae6419d2a4ee5589ddcbef5fa3880b15e4
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-08 13:24:19 -0500

    Completed the create_herd and create_fleet methods to make a list of 3 dinosaurs and 3 robots respectively.

--- fleet.py
+++ fleet.py
@@ -1,6 +1,12 @@
+from robot import Robot
+
+
 class Fleet:
     def __init__(self):
         self.robots = []
 
     def create_fleet(self):
-        pass
+        robot1 = Robot('R2-D2')
+        robot2 = Robot('WALL-E')
+        robot3 = Robot('Rosie')
+        self.robots = [robot1, robot2, robot3]
--- herd.py
+++ herd.py
@@ -1,6 +1,12 @@
+from dinosaur import Dinosaur
+
+
 class Herd:
     def __init__(self):
         self.dinosaurs = []
 
     def create_herd(self):
-        pass
+        dino1 = Dinosaur('tyrannosaurus rex', 30)
+        dino2 = Dinosaur('triceratops', 25)
+        dino3 = Dinosaur('velociraptor', 20)
+        self.dinosaurs = [dino1, dino2, dino3]
