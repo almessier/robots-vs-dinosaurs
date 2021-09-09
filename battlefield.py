5bab24ea1860e313c7d0fc34c5d2ccdedea9ce17
Alexander Messier <alexander.j.messier@gmail.com>
2021-09-09 10:45:18 -0500

    Added validation to user attacks and if enemy has no health, user is prompted to choose a different enemy. If an enemy is defeated by an attack, the user is made aware through a print to conole in the respective turn method.

--- battlefield.py
+++ battlefield.py
@@ -17,9 +17,18 @@ class Battlefield:
 
     def battle(self):
         while(True):
+            # Receive dinosaur attack from user and validate it
+            valid = False
+            while(valid == False):
+                user_dino_index = input(
+                    'Which dinosaur do you want to attack with? Type 1 for Tyrannosaurus rex, 2 for Triceratops, and 3 for Velociraptor: ')
+                if (user_dino_index != 1 or 2 or 3):
+                    print('Invalid input, Type 1, 2, or 3')
+                elif (self.herd.dinosaurs[int(user_dino_index) - 1].health <= 0):
+                    print('That dinosaur is dead! Pick another dinosaur.')
+                else:
+                    valid = True
 
-            user_dino_index = input(
-                'Which dinosaur do you want to attack with? Type 1 for Tyrannosaurus rex, 2 for Triceratops, and 3 for Velociraptor: ')
             user_dino = self.herd.dinosaurs[int(user_dino_index) - 1]
             self.dino_turn(user_dino)
 
@@ -31,8 +40,18 @@ class Battlefield:
             if count == 3:
                 break
 
-            user_robo_index = input(
-                'Which robot do you want to attack with? Type 1 for R2-D2, 2 for WALL-E, and 3 for Rosie: ')
+            # Receive robot attack from user and validate it
+            valid = False
+            while(valid == False):
+                user_robo_index = input(
+                    'Which robot do you want to attack with? Type 1 for R2-D2, 2 for WALL-E, and 3 for Rosie: ')
+                if (user_robo_index != 1 or 2 or 3):
+                    print('Invalid input, Type 1, 2, or 3')
+                elif (self.fleet.robots[int(user_robo_index) - 1].health <= 0):
+                    print('That robot is dead! Pick another robot.')
+                else:
+                    valid = True
+
             user_robo = self.fleet.robots[int(user_robo_index) - 1]
             self.robo_turn(user_robo)
 
@@ -50,8 +69,12 @@ class Battlefield:
         user_attack = input(': ')
         dinosaur.attack(self.fleet.robots[int(user_attack) - 1])
 
+        # Print results of attack
         print(
             f'{dinosaur.name} attacked {self.fleet.robots[int(user_attack) - 1].name} for {dinosaur.attack_power}')
+        if self.fleet.robots[int(user_attack) - 1].health <= 0:
+            print(
+                f'{self.fleet.robots[int(user_attack) - 1].name} has been defeated!')
 
     def robo_turn(self, robot):
         self.show_robo_opponent_options()
@@ -59,8 +82,12 @@ class Battlefield:
         user_attack = input(': ')
         robot.attack(self.herd.dinosaurs[int(user_attack) - 1])
 
+        # Print results of attack
         print(
             f'{robot.name} attacked {self.herd.dinosaurs[int(user_attack) - 1].name} for {robot.weapon.attack_power}')
+        if self.herd.dinosaurs[int(user_attack) - 1].health <= 0:
+            print(
+                f'{self.herd.dinosaurs[int(user_attack) - 1].name} has been defeated!')
 
     def show_dino_opponent_options(self):
         print('Attack! 1 for R2-D2, 2 for WALL-E, and 3 for Rosie.')
